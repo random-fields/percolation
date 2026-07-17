03a12487228fe2e750c8e3a36e3260185264a422
Yongxi Lin <aaronlin@andrew.cmu.edu>
2026-07-17 09:19:50 -0400

    feat: certify canonical replay chronology

--- Percolation.lean
+++ Percolation.lean
@@ -163,6 +163,7 @@ import Percolation.Critical.DynamicLaterSiteRuntimeHistory
 import Percolation.Critical.DynamicLaterSitePartition
 import Percolation.Critical.DynamicGlobalRevealBudget
 import Percolation.Critical.DynamicHistoryReplay
+import Percolation.Critical.DynamicReplayChronology
 import Percolation.Critical.DynamicHistoryReplayFinite
 import Percolation.Critical.DynamicReplayRevealBudget
 import Percolation.Critical.DynamicHistoryReplayStability
